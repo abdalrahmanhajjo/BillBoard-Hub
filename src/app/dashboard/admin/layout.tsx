32ef020d6e38fe8cf1233dc90571f2315fea7641
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2026-07-22 00:36:32 +0300

    Redirect unauthorized and unauthenticated users to specific routes according to auth status

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -1,7 +1,20 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { USER_ROLES } from "@/shared/constants/user-roles";
 export default async function AdminDashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    redirect("/login");
+  }
+
+  if (session.user.role !== USER_ROLES.ADMIN) {
+    redirect("/unauthorized");
+  }
+
   return <>{children}</>;
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,16 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
 export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    redirect("/login");
+  }
+
   return <>{children}</>;
 }
