57f6492ed2b3b24535716cf325b7b0d0fab8ce28
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2026-07-21 23:43:55 +0300

    Create user schema validations and types

---
+++ src/shared/contracts/user/user.schema.ts
@@ -0,0 +1,54 @@
+import { z } from "zod";
+import { USER_ROLES } from "@/shared/constants/user-roles";
+
+export const createUserSchema = z
+  .object({
+    firstName: z.string().trim().min(2, "First name is required."),
+    lastName: z.string().trim().min(2, "Last name is required."),
+    email: z.email("Please enter a valid email address.").trim().toLowerCase(),
+    role: z.enum(USER_ROLES),
+    password: z.string().min(8, "Password must be at least 8 characters."),
+    confirmPassword: z
+      .string()
+      .min(8, "Confirm password must be at least 8 characters."),
+  })
+  .refine((value) => value.password === value.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match.",
+  });
+
+export const updateUserInfoSchema = z
+  .object({
+    firstName: z.string().trim().min(2, "First name is required.").optional(),
+    lastName: z.string().trim().min(2, "Last name is required.").optional(),
+    email: z
+      .email("Please enter a valid email address.")
+      .trim()
+      .toLowerCase()
+      .optional(),
+  })
+  .refine(
+    (data) =>
+      Object.keys(data).length > 0 &&
+      Object.values(data).some((v) => v !== undefined),
+    {
+      message: "At least one field must be provided",
+    },
+  );
+
+export const resetPasswordSchema = z
+  .object({
+    password: z.string().min(8, "Password must be at least 8 characters."),
+    confirmPassword: z
+      .string()
+      .min(8, "Confirm password must be at least 8 characters."),
+  })
+  .refine((value) => value.password === value.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match.",
+  });
+
+
+export type UpdateUserInfoSchemaInput = z.input<typeof updateUserInfoSchema>;
+export type ResetPasswordSchemaInput = z.input<typeof resetPasswordSchema>;
+export type CreateUserSchemaInput = z.input<typeof createUserSchema>;
---
+++ src/shared/types/user.ts
@@ -0,0 +1,27 @@
+import { USER_ROLES } from "../constants/user-roles";
+import { UpdateUserInfoSchemaInput } from "../contracts/user/user.schema";
+
+export type UserRole = (typeof USER_ROLES)[keyof typeof USER_ROLES];
+
+export type User = {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: UserRole;
+  isActive: boolean;
+};
+
+export interface UpdateUserPayload {
+  id: string;
+  data: UpdateUserInfoSchemaInput;
+}
+
+export interface DeleteUserPayload {
+  id: string;
+}
+
+export interface GetUserPayload {
+  id: string;
+}
+
