d253e28c039d70fddf1ce47d0374bb6fc9a4511f
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2026-07-22 12:53:17 +0300

    Fix registration api

--- src/shared/contracts/auth/register.schema.ts
+++ src/shared/contracts/auth/register.schema.ts
@@ -1,12 +1,9 @@
 import { z } from "zod";
 
-import { USER_ROLES } from "@/shared/constants/user-roles";
-
 export const registerSchema = z.object({
     firstName: z.string().trim().min(2, "First name is required."),
     lastName: z.string().trim().min(2, "Last name is required."),
     email: z.email("Please enter a valid email address.").trim().toLowerCase(),
-    role: z.enum(USER_ROLES),
     password: z.string().min(8, "Password must be at least 8 characters."),
     confirmPassword: z
       .string()
