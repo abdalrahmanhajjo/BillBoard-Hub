6de70493a4280348e16c27bc481781b581997857
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2026-07-29 03:27:34 +0300

    Remove old ad creative modules and apis

--- src/shared/policies/index.ts
+++ src/shared/policies/index.ts
@@ -1,9 +1,7 @@
 import { billboardPolicy } from './modules/billboard-policy';
 import { campaignPolicy } from './modules/campaign-policy';
 import { advertiserProfilePolicy } from './modules/advertiser-profile-policy';
-
 import { bookingPolicy } from './modules/booking-policy';
-import { creativePolicy } from './modules/creative-policy';
 import { dashboardPolicy } from './modules/dashboard-policy';
 import { impressionPolicy } from './modules/impression-policy';
 import { playlistPolicy } from './modules/playlist-policy';
@@ -16,7 +14,6 @@ export const authorizationPolicy = {
   advertiserProfile: advertiserProfilePolicy,
   dashboard: dashboardPolicy,
   billboard: billboardPolicy,
-  creative: creativePolicy,
   playlist: playlistPolicy,
   schedule: schedulePolicy,
   impression: impressionPolicy,
